86dc4acfcd0aae1b5d1fb1adee6c0ccc51052af3
Rafal Kapuscik <rkapuscik@antmicro.com>
2020-02-12 13:52:06 +0100

    Use case statements (aesthetics only)
    Signed-off-by: Rafal Kapuscik <rkapuscik@antmicro.com>

--- src/UhdmAst.cpp
+++ src/UhdmAst.cpp
@@ -43,107 +43,120 @@ namespace UhdmAst {
     const unsigned int objectType = vpi_get(vpiType, obj_h);
     std::cout << "> type: " << objectType << std::endl;
 
-    if (objectType == vpiDesign) {
-      std::cout << "> Is a design" << std::endl;
-      itr = vpi_iterate(UHDM::uhdmallModules,obj_h);
-      while (vpiHandle vpi_obj = vpi_scan(itr) ) {
-        node = visit_object(vpi_obj);
-        std::cout << "! Out of design children" << std::endl;
-        vpi_free_object(vpi_obj);
-      }
-      vpi_free_object(itr);
-      return node;
-    }
-    else if (objectType == vpiClassObj) {
-      std::cout << "> Is a ClassObj" << std::endl;
-    }
-    else if (objectType == vpiPort) {
-      std::cout << "> Is a port" << std::endl;
-      AstPort *port = nullptr;
-      AstVar *var = nullptr;
-      AstBasicDType *dtype = nullptr;
-
-      dtype = new AstBasicDType(new FileLine("uhdm"), AstBasicDTypeKwd::LOGIC_IMPLICIT);
-
-      port = new AstPort(new FileLine("uhdm"), ++numPorts, objectName);
-
-      var = new AstVar(new FileLine("uhdm"), AstVarType::PORT, objectName, dtype);
-      if (const int n = vpi_get(vpiDirection, obj_h)) {
-        if (n == 1) {
-          var->declDirection(VDirection::INPUT);
-          var->direction(VDirection::INPUT);
-        } else if (n == 2) {
-          var->declDirection(VDirection::OUTPUT);
-          var->direction(VDirection::OUTPUT);
+    switch(objectType) {
+      case vpiDesign: {
+        std::cout << "> Is a design" << std::endl;
+        itr = vpi_iterate(UHDM::uhdmallModules,obj_h);
+        while (vpiHandle vpi_obj = vpi_scan(itr) ) {
+          node = visit_object(vpi_obj);
+          std::cout << "! Out of design children" << std::endl;
+          vpi_free_object(vpi_obj);
         }
+        vpi_free_object(itr);
+        return node;
       }
-
-      port->addNextNull(var);
-      var->childDTypep(dtype);
-
-      //if (v3Global.opt.trace())
-      //    var->trace(true);
-
-      if (port) {
-        return port;
+      case vpiClassObj: {
+        std::cout << "> Is a ClassObj" << std::endl;
+        break;
       }
-    }
-    else if (objectType == vpiPackage) {
-      std::cout << "> Is a package" << std::endl;
-    }
-    else if (objectType == vpiClassDefn) {
-      std::cout << "> Is a ClassDefn" << std::endl;
-    }
-    else if (objectType == vpiModule) {
-      std::cout << "> Is a module" << std::endl;
-      AstModule *module = new AstModule(new FileLine("uhdm"), objectName);
+      case vpiPort: {
+        std::cout << "> Is a port" << std::endl;
+        AstPort *port = nullptr;
+        AstVar *var = nullptr;
+        AstBasicDType *dtype = nullptr;
 
-      if (module != nullptr) {
-        std::cout << "> Have node" << std::endl;
+        dtype = new AstBasicDType(new FileLine("uhdm"), AstBasicDTypeKwd::LOGIC_IMPLICIT);
 
-        std::vector<int> module_child_nodes = {vpiPort, vpiContAssign};
-        for (auto child : module_child_nodes) {
-          itr = vpi_iterate(child, obj_h);
-          while (vpiHandle vpi_child_obj = vpi_scan(itr) ) {
-            auto *childNode = visit_object(vpi_child_obj);
-            std::cout << "! Out of general children" << std::endl;
-            if (childNode != nullptr) {
-              std::cout << ">> Has a child node" << std::endl;
-              // Update current module's list of statements
-              module->addStmtp(childNode);
-            }
-            vpi_free_object(vpi_child_obj);
+        port = new AstPort(new FileLine("uhdm"), ++numPorts, objectName);
+
+        var = new AstVar(new FileLine("uhdm"), AstVarType::PORT, objectName, dtype);
+        if (const int n = vpi_get(vpiDirection, obj_h)) {
+          if (n == 1) {
+            var->declDirection(VDirection::INPUT);
+            var->direction(VDirection::INPUT);
+          } else if (n == 2) {
+            var->declDirection(VDirection::OUTPUT);
+            var->direction(VDirection::OUTPUT);
           }
-          vpi_free_object(itr);
         }
-        return module;
-      }
-    } else if (objectType == vpiContAssign) {
-      std::cout << ">> Is a ContAssign" << std::endl;
-      AstNode* lvalue = nullptr;
-      AstNode* rvalue = nullptr;
 
-      // Right
-      itr = vpi_handle(vpiRhs,obj_h);
-      if (itr) {
-        std::cout << ">> have right" << std::endl;
-        rvalue = visit_object(itr);
-      }
-      vpi_free_object(itr);
+        port->addNextNull(var);
+        var->childDTypep(dtype);
 
-      // Left
-      itr = vpi_handle(vpiLhs,obj_h);
-      if (itr) {
-        std::cout << ">> have left" << std::endl;
-        lvalue = visit_object(itr);
-      }
-      vpi_free_object(itr);
+        //if (v3Global.opt.trace())
+        //    var->trace(true);
 
-      if (lvalue && rvalue) {
-        std::cout << ">> returning contAssign" << std::endl;
-        return new AstAssignW(new FileLine("uhdm"), lvalue, rvalue);
+        if (port) {
+          return port;
+        }
+        break;
+      }
+      case vpiPackage: {
+        std::cout << "> Is a package" << std::endl;
+        break;
+      }
+      case vpiClassDefn: {
+        std::cout << "> Is a ClassDefn" << std::endl;
+        break;
+      }
+      case vpiModule: {
+        std::cout << "> Is a module" << std::endl;
+        AstModule *module = new AstModule(new FileLine("uhdm"), objectName);
+
+        if (module != nullptr) {
+          std::cout << "> Have node" << std::endl;
+
+          std::vector<int> module_child_nodes = {vpiPort, vpiContAssign};
+          for (auto child : module_child_nodes) {
+            itr = vpi_iterate(child, obj_h);
+            while (vpiHandle vpi_child_obj = vpi_scan(itr) ) {
+              auto *childNode = visit_object(vpi_child_obj);
+              std::cout << "! Out of general children" << std::endl;
+              if (childNode != nullptr) {
+                std::cout << ">> Has a child node" << std::endl;
+                // Update current module's list of statements
+                module->addStmtp(childNode);
+              }
+              vpi_free_object(vpi_child_obj);
+            }
+            vpi_free_object(itr);
+          }
+          return module;
+        }
+        break;
+      }
+      case vpiContAssign: {
+        std::cout << ">> Is a ContAssign" << std::endl;
+        AstNode* lvalue = nullptr;
+        AstNode* rvalue = nullptr;
+
+        // Right
+        itr = vpi_handle(vpiRhs,obj_h);
+        if (itr) {
+          std::cout << ">> have right" << std::endl;
+          rvalue = visit_object(itr);
+        }
+        vpi_free_object(itr);
+
+        // Left
+        itr = vpi_handle(vpiLhs,obj_h);
+        if (itr) {
+          std::cout << ">> have left" << std::endl;
+          lvalue = visit_object(itr);
+        }
+        vpi_free_object(itr);
+
+        if (lvalue && rvalue) {
+          std::cout << ">> returning contAssign" << std::endl;
+          return new AstAssignW(new FileLine("uhdm"), lvalue, rvalue);
+        }
+        break;
+      }
+      default: {
+        break;
       }
     }
+
     return nullptr;
   }
 
