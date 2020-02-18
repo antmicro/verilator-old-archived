755d4c391ecdde6cb525a3ef90178989ef23d17c
Rafal Kapuscik <rkapuscik@antmicro.com>
2020-02-18 14:17:51 +0100

    Walk through all VPI relationships, even if they are not handled yet
    Signed-off-by: Rafal Kapuscik <rkapuscik@antmicro.com>

--- src/UhdmAst.cpp
+++ src/UhdmAst.cpp
@@ -67,6 +67,7 @@ namespace UhdmAst {
     switch(objectType) {
       case vpiDesign: {
 
+        // Unhandled relationships: will visit (and print) the object
         visit_one_to_many({UHDM::uhdmtopModules,
                            UHDM::uhdmallPrograms,
                            UHDM::uhdmallPackages,
@@ -92,6 +93,18 @@ namespace UhdmAst {
         AstPort *port = nullptr;
         AstVar *var = nullptr;
         AstBasicDType *dtype = nullptr;
+
+        // Unhandled relationships: will visit (and print) the object
+        visit_one_to_many({vpiBit},
+                          obj_h,
+                          [](AstNode*){});
+        visit_one_to_one({vpiTypedef,
+                          vpiInstance,
+                          vpiModule,
+                          vpiHighConn,
+                          vpiLowConn},
+                         obj_h,
+                         [](AstNode*){});
 
         dtype = new AstBasicDType(new FileLine("uhdm"),
                                   AstBasicDTypeKwd::LOGIC_IMPLICIT);
@@ -129,6 +142,58 @@ namespace UhdmAst {
       case vpiModule: {
         AstModule *module = new AstModule(new FileLine("uhdm"), objectName);
 
+        // Unhandled relationships: will visit (and print) the object
+        visit_one_to_many({vpiProcess,
+                           vpiPrimitive,
+                           vpiPrimitiveArray,
+                           vpiInterface,
+                           vpiInterfaceArray,
+                           vpiModule,
+                           vpiModuleArray,
+                           vpiModPath,
+                           vpiTchk,
+                           vpiDefParam,
+                           vpiIODecl,
+                           vpiAliasStmt,
+                           vpiClockingBlock,
+                           vpiTaskFunc,
+                           vpiNet,
+                           vpiArrayNet,
+                           vpiAssertion,
+                           vpiClassDefn,
+                           vpiProgram,
+                           vpiProgramArray,
+                           vpiSpecParam,
+                           vpiConcurrentAssertions,
+                           vpiVariables,
+                           vpiParameter,
+                           vpiInternalScope,
+                           vpiTypedef,
+                           vpiPropertyDecl,
+                           vpiSequenceDecl,
+                           vpiNamedEvent,
+                           vpiNamedEventArray,
+                           vpiVirtualInterfaceVar,
+                           vpiReg,
+                           vpiRegArray,
+                           vpiMemory,
+                           vpiLetDecl,
+                           vpiImport
+                          },
+                          obj_h,
+                          [](AstNode* node){});
+        visit_one_to_one({vpiDefaultDisableIff,
+                          vpiInstanceArray,
+                          vpiGlobalClocking,
+                          vpiDefaultClocking,
+                          vpiModuleArray,
+                          vpiInstance,
+                          vpiModule  // TODO: Both here and in one-to-many?
+                         },
+                         obj_h,
+                         [](AstNode*){});
+
+
         if (module != nullptr) {
           visit_one_to_many({vpiPort, vpiContAssign, vpiLogicNet},
               obj_h,
@@ -143,6 +208,14 @@ namespace UhdmAst {
       case vpiContAssign: {
         AstNode* lvalue = nullptr;
         AstNode* rvalue = nullptr;
+
+        // Unhandled relationships: will visit (and print) the object
+        visit_one_to_one({vpiDelay},
+                         obj_h,
+                         [](AstNode*){});
+        visit_one_to_many({vpiBit},
+                          obj_h,
+                          [](AstNode*){});
 
         // Right
         visit_one_to_one({vpiRhs},
@@ -165,6 +238,7 @@ namespace UhdmAst {
       }
       case vpiRefObj: {
         bool isLvalue = false;
+        // Unhandled relationships: will visit (and print) the object
         visit_one_to_one({vpiInstance,
                           vpiTaskFunc,
                           vpiTypespec},
@@ -198,12 +272,29 @@ namespace UhdmAst {
       case vpiLogicNet: {
         // Handling of this node is not functional yet
         break;
-          visit_one_to_one({vpiLeftRange, vpiRightRange},
-                           obj_h,
-                           [](AstNode*){});
-          visit_one_to_many({vpiRange, vpiBit},
-                            obj_h,
-                            [](AstNode*){});
+        // Unhandled relationships: will visit (and print) the object
+        visit_one_to_one({vpiLeftRange,
+                          vpiRightRange,
+                          vpiSimNet,
+                          vpiModule,
+                          vpiTypespec
+                         },
+                         obj_h,
+                         [](AstNode*){});
+        visit_one_to_many({vpiRange,
+                           vpiBit,
+                           vpiPortInst,
+                           vpiDriver,
+                           vpiLoad,
+                           vpiLocalDriver,
+                           vpiLocalLoad,
+                           vpiPrimTerm,
+                           vpiContAssign,
+                           vpiPathTerm,
+                           vpiTchkTerm
+                          },
+                          obj_h,
+                          [](AstNode*){});
 
         AstBasicDType *dtype = nullptr;
         dtype = new AstBasicDType(new FileLine("uhdm"),
@@ -222,6 +313,7 @@ namespace UhdmAst {
           std::cout << "|vpiFullName: " << s << std::endl;
         }
 
+        // Unhandled relationships: will visit (and print) the object
         visit_one_to_many({vpiConcurrentAssertions,
                            vpiVariables,
                            vpiParameter,
