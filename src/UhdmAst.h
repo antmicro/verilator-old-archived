57d3e45f159a02e7b9cb8349d0c67e55e301b3a5
Rafal Kapuscik <rkapuscik@antmicro.com>
2020-02-18 13:43:12 +0100

    Refactor to avoid code duplication
    Signed-off-by: Rafal Kapuscik <rkapuscik@antmicro.com>

--- src/UhdmAst.h
+++ src/UhdmAst.h
@@ -8,7 +8,11 @@
 #include "V3Ast.h"
 
 namespace UhdmAst {
+
+  // Visits single VPI object and creates proper AST node
   AstNode* visit_object (vpiHandle obj_h);
+
+  // Visits all VPI design objects and returns created ASTs
   std::vector<AstNodeModule*> visit_designs (const std::vector<vpiHandle>& designs);
 }
 #endif  // Guard
