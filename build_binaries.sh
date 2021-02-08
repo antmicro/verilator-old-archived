0d554ae02cc30cb78f30ad7f24a228992afacffe
Kamil Rakoczy <krakoczy@antmicro.com>
2021-02-08 09:19:04 +0100

    Download yosys files and test vcddiff
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- build_binaries.sh
+++ build_binaries.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd Surelog && make PREFIX=$PWD/../image/ release install -j $(nproc) && cd ..
 autoconf && ./configure --prefix=$PWD/image && make install
+make -C vcddiff PREFIX=./image -j $(nproc)
+cp -p vcddiff/vcddiff image/bin/vcddiff
