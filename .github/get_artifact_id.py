395b4f697f1c20ecbb01cee4df45a6ee15066807
Kamil Rakoczy <krakoczy@antmicro.com>
2021-02-11 13:47:20 +0100

    Use github-scripts to download artifact
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- .github/get_artifact_id.py
+++
@@ -1,7 +0,0 @@
-import sys, json
-json_file = json.load(sys.stdin);
-for i in range(len(json_file['artifacts'])):
-    if json_file['artifacts'][i]["name"] == sys.argv[1]:
-        pos = i
-        break
-print(json_file['artifacts'][pos]["id"])
