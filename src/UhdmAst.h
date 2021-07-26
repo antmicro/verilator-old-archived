393a329cc0775ca27637fd4e20f24cc94a67db2d
Ryszard Różak <rrozak@antmicro.com>
2021-07-26 15:29:49 +0200

    Move duplicate handling to seprate function
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

--- src/UhdmAst.h
+++ src/UhdmAst.h
@@ -17,8 +17,8 @@ struct UhdmShared {
     std::map<std::string, AstPackage*> package_map;
     std::string package_prefix;
     NameNodeMap partial_modules;
-    std::unordered_map<const UHDM::BaseClass*, std::string> visited_types;
-    std::unordered_map<const UHDM::BaseClass*, std::string> visited_variables;
+    std::unordered_map<const UHDM::BaseClass*, std::string> visited_types_map;
+    std::unordered_set<const UHDM::BaseClass*> visited_objects;
     // Store parameters here (values can be updated for each instance)
     // Final values will be added in respective module/package
     std::map<std::string, NameNodeMap> top_param_map;
