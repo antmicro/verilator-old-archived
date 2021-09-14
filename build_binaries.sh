976d17edd61245b8556b247178e1197083f7aa7b
Ryszard Różak <rrozak@antmicro.com>
2021-09-14 13:51:20 +0200

    Extract image directory path to variable
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

--- build_binaries.sh
+++ build_binaries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
 cd Surelog && make PREFIX=$PWD/../image/ release install -j $(nproc) && cd ..
-autoconf && ./configure --prefix=$PWD/image && make -j $(nproc) && make install
+autoconf && ./configure --prefix=$PWD/image IMAGE_DIR=$PWD/image && make -j $(nproc) && make install
 make -C vcddiff PREFIX=./image -j $(nproc)
 cp -p vcddiff/vcddiff image/bin/vcddiff
