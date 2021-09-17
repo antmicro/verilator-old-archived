3975a0494b5506bc44a052d9b3be43f878d4528b
Ryszard Różak <rrozak@antmicro.com>
2021-09-17 11:58:20 +0200

    Set Surelog's PREFIX to SURELOG_INSTALL_DIR
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

--- build_binaries.sh
+++ build_binaries.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
-cd Surelog && make PREFIX=$PWD/../image/ release install -j $(nproc) && cd ..
-autoconf && ./configure --prefix=$PWD/image SURELOG_INSTALL_DIR=$PWD/image && make -j $(nproc) && make install
+export SURELOG_INSTALL_DIR=$PWD/image
+cd Surelog && make PREFIX=$SURELOG_INSTALL_DIR release install -j $(nproc) && cd ..
+autoconf && ./configure --prefix=$PWD/image && make -j $(nproc) && make install
 make -C vcddiff PREFIX=./image -j $(nproc)
 cp -p vcddiff/vcddiff image/bin/vcddiff
