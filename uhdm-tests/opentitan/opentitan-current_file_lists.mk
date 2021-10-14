11ce9fc53bf055b087048dd3c7e09a0765d6d445
Ryszard Różak <rrozak@antmicro.com>
2021-10-14 12:44:54 +0200

    [WIP] Update opentitan-current modules lists
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

--- uhdm-tests/opentitan/opentitan-current_file_lists.mk
+++ uhdm-tests/opentitan/opentitan-current_file_lists.mk
@@ -1,15 +1,7 @@
 # Filenames starting with / to avoid false matches by suffix
 
 VERILATOR_ONLY_SOURCE_FILENAMES=\
-/lc_ctrl.sv
+/prim_lfsr.sv \
+/otbn_rnd.sv
 
 VERILATOR_AND_SURELOG_SOURCE_FILENAMES=\
-/prim_lc_sync.sv \
-/prim_flop_2sync.sv \
-/prim_generic_flop_2sync.sv \
-/prim_flop.sv \
-/prim_generic_flop.sv \
-/prim_alert_sender.sv \
-/prim_fifo_sync.sv \
-/prim_buf.sv \
-/prim_diff_decode
