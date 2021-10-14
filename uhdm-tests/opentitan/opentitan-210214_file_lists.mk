f04f9692a60cb61c275dc75ad764cce6a477ed16
Ryszard Różak <rrozak@antmicro.com>
2021-10-14 11:31:53 +0200

    Move modules lists to separate files according to opentitan version
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

---
+++ uhdm-tests/opentitan/opentitan-210214_file_lists.mk
@@ -0,0 +1,15 @@
+# Filenames starting with / to avoid false matches by suffix
+
+VERILATOR_ONLY_SOURCE_FILENAMES=\
+/lc_ctrl.sv
+
+VERILATOR_AND_SURELOG_SOURCE_FILENAMES=\
+/prim_lc_sync.sv \
+/prim_flop_2sync.sv \
+/prim_generic_flop_2sync.sv \
+/prim_flop.sv \
+/prim_generic_flop.sv \
+/prim_alert_sender.sv \
+/prim_fifo_sync.sv \
+/prim_buf.sv \
+/prim_diff_decode
