877057c10729084eec88f4f2ee2b9db0ee4b5952
Ryszard Różak <rrozak@antmicro.com>
2022-01-18 15:59:51 +0100

    Parse all opentitan-210214 modules by uhdm-verilator
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

--- uhdm-tests/opentitan/opentitan-210214_file_lists.mk
+++ uhdm-tests/opentitan/opentitan-210214_file_lists.mk
@@ -1,20 +1,5 @@
 # Filenames starting with / to avoid false matches by suffix
 
 VERILATOR_ONLY_SOURCE_FILENAMES=\
-/top_earlgrey.sv \
-/prim_generic_rom.sv \
-/prim_rom.sv \
-/prim_rom_adv.sv \
 
 VERILATOR_AND_SURELOG_SOURCE_FILENAMES=\
-/tlul_adapter_sram.sv \
-/lc_ctrl.sv \
-/prim_lc_sync.sv \
-/prim_flop_2sync.sv \
-/prim_generic_flop_2sync.sv \
-/prim_flop.sv \
-/prim_generic_flop.sv \
-/prim_alert_sender.sv \
-/prim_fifo_sync.sv \
-/prim_buf.sv \
-/prim_diff_decode
